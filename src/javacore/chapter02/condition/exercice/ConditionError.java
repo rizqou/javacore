418f366d74d9365c6993003e6b0fc71709b05a28
hussein said <alihusseinsaid@yahoo.be>
2025-03-06 17:32:54 +0100

    exercice ConditionError

---
+++ src/javacore/chapter02/condition/exercice/ConditionError.java
@@ -0,0 +1,24 @@
+package javacore.chapter02.condition.exercice;
+
+public class ConditionError {
+
+
+    public static void main(String[] args) {
+
+        int age = 80;
+
+        if (age <= 16) {
+            System.out.println("Vous êtes mineur.");
+        } else if (age == 17) {
+            System.out.println("Vous êtes bientôt majeur !");
+        } else if (age == 18) {
+            System.out.println("Vous êtes majeur.");
+        } else if (age < 60) {
+            System.out.println("Vous êtes un adulte.");
+        } else if (age < 100) {
+            System.out.println("Vous n'êtes plus tout jeune.");
+        } else {
+            System.out.println("Vous êtes une exception dans ce monde !");
+        }
+    }
+}
